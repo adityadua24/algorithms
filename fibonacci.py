474a098155c3205563c1af3216b543cbec3cb697
Aditya Dua <aditya.dua@firstwavecloud.com>
2020-07-21 16:05:38 +1000

    fibonacci updated

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,12 @@
 import time
 
-def fib1(n):
+def inefficient(n):
     if n <= 1:
         return n
     else:
-        return fib1(n-1) + fib1(n-2)
+        return inefficient(n-1) + inefficient(n-2)
 
-def fib2(n):
+def efficient(n):
     x = []
     x.append(0)
     x.append(1)
@@ -18,9 +18,8 @@ def fib2(n):
 if __name__ == "__main__":
     from sys import argv
     n = int(argv[1])
-    print("Using in-efficient algorithm")
-    for f in [fib1, fib2]:
-        print("Using %s" % f.__name__)
+    for f in [inefficient, efficient]:
+        print("Using %s algorithm..." % f.__name__)
         start_time = time.time()
         num = f(n)
         end_time = time.time()
